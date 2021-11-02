f8fa95a5e776f5c5c13212b462749ad7730cb22a
Niklavs <niklavsricardssablis@gmail.com>
2021-11-02 17:41:27 +0200

    last commit

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -8,16 +8,13 @@ use Illuminate\Support\Facades\DB;
 class ActivityController extends Controller
 {
 
-    public function get(Request $request)
+    public function get()
     {
-        $dateFrom = $request->query('dateFrom') ?? '';
-        $dateTo = $request->query('dateTo') ?? '';
-
         $activities = DB::table('activity_log')
-            ->havingBetween('number_of_orders', [$dateFrom, $dateTo])
-            ->limit(5)
+            ->latest()
+            ->limit(10)
             ->get();
 
-
+        return response()->json($activities);
     }
 }
